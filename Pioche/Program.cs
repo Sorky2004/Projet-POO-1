7f23eec9fb3316d1d5b18901a3de1416fce5f375
Sorky2004 <louison.jacquin123@gmail.com>
2025-10-19 14:27:24 -0400

    Update Program.cs

--- Pioche/Program.cs
+++ Pioche/Program.cs
@@ -7,11 +7,9 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Player p1 = new Player("Doe", "John");
-            Player p2 = new Player("Smith", "Jane");
+            
 
-            Console.WriteLine(p1.ToString());
-            Console.WriteLine(p2.ToString());
+
         }
     }
 }
